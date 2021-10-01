ca45905a1613edf8226cc8a70eef9dc4f7d56901
MariFon <87157578+MariFon@users.noreply.github.com>
2021-10-01 19:50:35 -0300

    site mentalista finalizado
    contendo criterios finalizados

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ function Chutar() {
     // console.log("Função chutar")
 
     //sortear um numero aleatorio secreto
-    let numeroSecreto = 3
-
+    let numeroSecreto = parseInt(Math.random() * 11)
+    console.log(numeroSecreto)
     // pegar o numero digitado e converter para numero inteiro
     let chute = parseInt(document.querySelector("#valor").value)
 
@@ -14,7 +14,9 @@ function Chutar() {
         //se estiver correto e os numeros forem iguais, mostrar a mensagem que a pessoa acertou
         document.querySelector(".resultado").innerHTML = "VOCÊ ACERTOU"
     }
-    else {
+    else //senão
+
+    {
         //se tiver errado e os numeros forem diferentes, mostrar a mensagem que a pessoa errou
         document.querySelector(".resultado").innerHTML = "VOCÊ ERROU"
     }
